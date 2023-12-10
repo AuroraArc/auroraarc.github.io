c8e5b78b4e9f169f62c5762da152db50dd2aecbe
AuroraArc <33708423+AuroraArc@users.noreply.github.com>
2023-12-09 19:13:59 -0500

    update config

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -4,7 +4,7 @@ const config = {
   github: {
     username: 'AuroraArc', // Your GitHub org/user name. (Required)
     sortBy: 'updated', // stars | updated
-    limit: 3, // How many projects to display.
+    limit: 2, // How many projects to display.
     exclude: {
       forks: true, // Forked projects will not be displayed if set to true.
       projects: ['data', 'auroraarc', 'LaTeX', 'qubit-feature-analysis'], // These projects will not be displayed. example: ['my-project1', 'my-project2']
