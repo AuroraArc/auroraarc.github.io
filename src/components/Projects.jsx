4fe29f2c301589b7719af8e9ac536e8ba1f1a2ce
AuroraArc <33708423+AuroraArc@users.noreply.github.com>
2025-01-15 16:52:39 -0600

    update projects page

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,25 @@
+import { useEffect, useState } from 'react';
+import '../assets/index.css'; // Import the CSS file
+
 const Projects = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(false);
+  }, []);
+
   return (
-    <div>
-      <h1>Projects</h1>
-      <p>Here are some of my projects...</p>
+    <div className={`fade-in h-screen bg-base-300`}>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <div className="p-4 lg:p-10 min-h-full">
+          <h1 className="text-2xl font-bold text-base-content">Projects</h1>
+          <p className="text-base-content text-opacity-70">
+            Here are some of my projects...
+          </p>
+        </div>
+      )}
     </div>
   );
 };
