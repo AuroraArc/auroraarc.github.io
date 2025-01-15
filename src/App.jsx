17463822a949660f19dfbba9c6f045ad27c82af1
AuroraArc <33708423+AuroraArc@users.noreply.github.com>
2025-01-15 16:26:45 -0600

    add gitprofile config

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import GitProfile from './components/GitProfile';
+import config from '../gitprofile.config';
 import Header from './components/Header';
 import Projects from './components/Projects';
 
@@ -16,13 +17,3 @@ function App() {
 }
 
 export default App;
-
-
-// import config from '../gitprofile.config';
-// import GitProfile from './components/GitProfile';
-
-// function App() {
-//   return <GitProfile config={config} />;
-// }
-
-// export default App;
