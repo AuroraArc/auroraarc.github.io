e3c4cfd5a9f4ebc3034e02431a0b82792a3e0496
AuroraArc <33708423+AuroraArc@users.noreply.github.com>
2025-01-15 17:25:28 -0600

    asdsaDA

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,15 +3,17 @@ import '../assets/index.css'; // Import the CSS file
 
 const Header = ({ theme }) => {
   return (
-    <header className={`header ${theme} lighter-header`}>
-      <nav className="nav-container">
-        <div className="nav-links">
-          <Link to="/">Home</Link>
-          <Link to="/projects">Projects</Link>
-        </div>
-      </nav>
+    <>
+      <header className={`header ${theme} lighter-header`}>
+        <nav className="nav-container">
+          <div className="nav-links">
+            <Link to="/">Home</Link>
+            <Link to="/projects">Projects</Link>
+          </div>
+        </nav>
+      </header>
       <div className="separator-bar"></div>
-    </header>
+    </>
   );
 };
 
