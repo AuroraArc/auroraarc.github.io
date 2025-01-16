59a2ae6e222324d7b57685ea915df78b33a28431
AuroraArc <33708423+AuroraArc@users.noreply.github.com>
2025-01-15 22:22:28 -0600

    start at top

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,19 @@ import Projects from './components/Projects';
 import QMLDFT from './components/project-details/QMLDFT';
 import UnderstandingQubitFeatures from './components/project-details/UnderstandingQubitFeatures';
 import Recidivism from './components/project-details/Recidivism';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Header theme={config.themeConfig.defaultTheme} />
       <Routes>
         <Route path="/" element={<GitProfile config={config} />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/projects/quantum-computing-and-ml-for-dft-calculations-in-proteins" element={<QMLDFT />} />
         <Route path="/projects/understanding-the-influence-of-qubit-features-on-error-rates" element={<UnderstandingQubitFeatures />} />
-        <Route path="/projects/breaking-the-cycle:-reducing-recidivism-in-iowa-state-prisons" element={<Recidivism />} />
+        <Route path="/projects/breaking-the-cycle-reducing-recidivism-in-iowa-state-prisons" element={<Recidivism />} />
       </Routes>
     </Router>
   );
