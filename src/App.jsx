5fa3787daa938cbf3dd4db4dbc3e058f17379836
AuroraArc <33708423+AuroraArc@users.noreply.github.com>
2025-01-16 01:28:03 -0600

    organize files

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import GitProfile from './components/GitProfile';
 import config from '../gitprofile.config';
 import Header from './components/Header';
 import Projects from './components/Projects';
-import QMLDFT from './components/project-details/QMLDFT';
-import UnderstandingQubitFeatures from './components/project-details/UnderstandingQubitFeatures';
-import Recidivism from './components/project-details/Recidivism';
+import QMLDFT from './components/project-details/qmlft/QMLDFT';
+import UnderstandingQubitFeatures from './components/project-details/qubit-features/UnderstandingQubitFeatures';
+import Recidivism from './components/project-details/recidivism/Recidivism';
 import ScrollToTop from './components/ScrollToTop';
 
 function App() {
